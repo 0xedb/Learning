849306deb03d99edf079062d2b92327904ab7c76
theBashShell <github.com/theBashShell>
2018-09-02 10:16:10 +0000

    changed settings to see if things work out

---
+++ Electron-JS/bootstrap.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({ width: 1000, height: 800 });
+    win.loadFile('index.html');
+
+    win.on('closed', () => {
+        win = null;
+    });
+
+    //win.openDevTools();
+}
+
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    app.quit();
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow();
+    }
+});
